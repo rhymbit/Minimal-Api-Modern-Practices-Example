705773d0facab61e37ca053ce8ed3dfc20770520
prateek332 <prateekn332@gmail.com>
2023-01-25 16:51:00 +0530

    Created User Model & a Database using EfCore

---
+++ Database/DbContext.cs
@@ -0,0 +1,26 @@
+﻿using Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database;
+
+public class DbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public DbSet<User> Users { get; }
+
+    public string DbPath { get; }
+
+    public DbContext() =>
+        DbPath = Path.Combine(Environment.CurrentDirectory, "database.db");
+
+    public DbContext(DbContextOptions<DbContext> options) :
+        base(options) =>
+        DbPath = Path.Combine(Environment.CurrentDirectory, "database.db");
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+      options.UseSqlite($"Data Source={DbPath}");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+      modelBuilder.Entity<User>().ToTable("Users")
+        .HasKey(u => u.Id)
+        .HasName("PK_Users");
+}
---
+++ Database/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Database.Models;
+
+public class User
+{
+    public string Id;
+    public string Name;
+    public UInt16 Age;
+    public string Email;
+    public string Password;
+}
